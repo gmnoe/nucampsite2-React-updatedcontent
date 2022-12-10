4f0ff34abea931588f226eca137fa54b9175d4ad
Greg Noethlich <gmnoethlich@gmail.com>
2022-12-10 13:24:17 -0600

    Campsites useSelector

--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -1,16 +1,17 @@
 import { Col, Row } from 'reactstrap';
 // import DisplayCard from './DisplayCard';
+import { useSelector } from 'react-redux';
 import AnimatedDisplayCard from './AnimatedDisplayCard';
 import { selectFeaturedCampsite } from '../campsites/campsitesSlice';
 import { selectFeaturedPromotion } from '../promotions/promotionsSlice';
 import { selectFeaturedPartner } from '../partners/partnersSlice';
 
 const DisplayList = () => {
-    const items = [
-        selectFeaturedCampsite(),
-        selectFeaturedPromotion(),
-        selectFeaturedPartner()
-    ];
+    const items = useSelector((state) => [
+        selectFeaturedCampsite(state),
+        selectFeaturedPromotion(state),
+        selectFeaturedPartner(state)
+    ]);
 
     return (
         <Row>
